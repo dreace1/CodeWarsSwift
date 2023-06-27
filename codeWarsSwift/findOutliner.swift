5dc8d0371a48310560f464c8d654ef2ea1e7a8b1
Hannes <hannesro27@gmail.com>
2023-06-27 10:06:50 +0200

    project rename

---
+++ codeWarsSwift/findOutliner.swift
@@ -0,0 +1,23 @@
+//
+//  findOutliner.swift
+//  codeWarsSwift
+//
+//  Created by Hannes Rosenkranz on 31.01.23.
+//
+
+import Foundation
+
+func findOutlier(_ array: [Int]) -> Int {
+    
+    for number in array {
+        if(number % 2 == 0){
+            
+        }
+        
+    }
+    
+    
+    
+    return 0
+}
+
