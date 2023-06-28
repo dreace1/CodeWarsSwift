b5463614764978659162bfabdfa83053913a8902
Hannes <hannesro27@gmail.com>
2023-06-28 09:44:11 +0200

    finished spacify kata

---
+++ CodeWarsSwift/Spacify.swift
@@ -0,0 +1,19 @@
+//
+//  ConsecutiveStrings.swift
+//  CodeWarsSwift
+//
+//  Created by Hannes Rosenkranz on 27.06.23.
+//
+
+import Foundation
+
+func longestConsec(_ strarr: [String], _ k: Int) -> String {
+    var i = 0
+    var j = i
+    for i in 0...strarr.count {
+        for j in 0...strarr.count{
+            
+        }
+        
+    }
+}
